7f6e3913dc04f29bf88ff7ce334bdd0e6d3d2d15
Jhulan Dey <187887482+JhulanD@users.noreply.github.com>
2026-09-10 12:11:42 +0530

    Add Python-Codebasics

---
+++ Python-Codebasics/pythonBasics.py
@@ -0,0 +1,38 @@
+# # Python Basics
+#
+# # Variables
+# #
+#
+#
+# # Numbers
+# food =  120.25
+# rent = 500
+# utilities = 2000
+#
+# total_cost = food + rent + utilities
+#
+# print(round(total_cost,1) )
+# print(total_cost)
+#
+# # Convert strings to floats
+# total = float(food) + float(rent) + float(utilities)
+# print(total)
+#
+#
+# # Strings
+# first = "Mohan"
+# last = "Yen"
+# name = first + " " + last
+# print(type(first))
+# print(first + last)
+#
+# age = 20
+#
+# info = f"{first} + {last} is {age} years old."
+# print(info)
+#
+# print(name[-6])
+#
+# # =========================================================
+
+    
